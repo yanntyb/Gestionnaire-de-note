4cdf96db656c7b8522bb05e201a01e9a46944703
yanntyb <yanntyb.lbc@gmail.com>
2022-01-17 11:14:50 +0000

    base

--- public/index.php
+++ public/index.php
@@ -7,6 +7,5 @@ use Yanntyb\Router\Model\Classes\Route;
 require "../vendor/autoload.php";
 
 $router = new Router(new Route("home", "/", [HomeController::class, "home"]));
-$router->addRoute("test", "/test", [HomeController::class, "message"]);
 
 $router->handleQuery();
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,10 @@ namespace Yanntyb\Projet\Controller;
 
 use Yanntyb\Controller\Model\Classes\Controller;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller{
     public function home(){
-        echo "ok";
-    }
-
-    public function message($message){
-        echo $message;
+        $this->render("home.html.twig", [
+            "title" => "Home"
+        ]);
     }
 }
